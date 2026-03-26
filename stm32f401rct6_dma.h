8da053f7d926a058a81c18b194980b0a453f18b4
Vladyslav Kazachenko <vladyslav.kazachenko@gmail.com>
2026-03-26 09:42:16 +0100

    dma: add LIFCR_* and HIFCR_*

--- stm32f401rct6_dma.h
+++ stm32f401rct6_dma.h
@@ -50,7 +50,51 @@ struct dma_t {
     HISR_TCIF7,
   };
   unsigned int LIFCR;
+  enum {
+    LIFCR_CFEIF0 = 0,
+    LIFCR_CDMEIF0 = 2,
+    LIFCR_CTEIF0,
+    LIFCR_CHTIF0,
+    LIFCR_CTCIF0,
+    LIFCR_CFEIF1 = 6,
+    LIFCR_CDMEIF1 = 8,
+    LIFCR_CTEIF1,
+    LIFCR_CHTIF1,
+    LIFCR_CTCIF1,
+    LIFCR_CFEIF2 = 16,
+    LIFCR_CDMEIF2 = 18,
+    LIFCR_CTEIF2,
+    LIFCR_CHTIF2,
+    LIFCR_CTCIF2,
+    LIFCR_CFEIF3 = 22,
+    LIFCR_CDMEIF3 = 24,
+    LIFCR_CTEIF3,
+    LIFCR_CHTIF3,
+    LIFCR_CTCIF3,
+  };
   unsigned int HIFCR;
+  enum {
+    HIFCR_CFEIF4 = 0,
+    HIFCR_CDMEIF4 = 2,
+    HIFCR_CTEIF4,
+    HIFCR_CHTIF4,
+    HIFCR_CTCIF4,
+    HIFCR_CFEIF5 = 6,
+    HIFCR_CDMEIF5 = 8,
+    HIFCR_CTEIF5,
+    HIFCR_CHTIF5,
+    HIFCR_CTCIF5,
+    HIFCR_CFEIF6 = 16,
+    HIFCR_CDMEIF6 = 18,
+    HIFCR_CTEIF6,
+    HIFCR_CHTIF6,
+    HIFCR_CTCIF6,
+    HIFCR_CFEIF7 = 22,
+    HIFCR_CDMEIF7 = 24,
+    HIFCR_CTEIF7,
+    HIFCR_CHTIF7,
+    HIFCR_CTCIF7,
+  };
   unsigned int S0CR;
   unsigned int S0NDTR;
   unsigned int S0PAR;
